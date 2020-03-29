b7a4439e0244c0841e255766dee4d20c7a7e5035
Ivan Martin Alonso <ivan.martin.alonso.89@gmail.com>
2020-03-29 01:51:11 +0100

    Adding MariaDb connector to write events in database

--- PFG-Consumer-2.py
+++ PFG-Consumer-2.py
@@ -1,15 +1,37 @@
 from kafka import KafkaConsumer
+import mysql.connector as mariadb
+import schedule
 
+def connectDatabase():
+    mariadb_connection = mariadb.connect(user='root', password='root', database='PFG-DATABASE')
+    cursor = mariadb_connection.cursor()
 
-def main():
+    return mariadb_connection,cursor
+
+def printProducts():
+    print("Mostrando los productos de la base de datos")
+    cursor.execute("SELECT Productos FROM Eventos ")
+    for Productos in cursor:
+        print(Productos)
+
+def main(mariadb_connection,cursor):
+
+    bootstrap_servers = ['localhost:9092']
+    topicName = 'test_1'
     while(True):
-        bootstrap_servers = ['localhost:9092']
-        topicName = 'test_1'
+        #schedule.every(2).seconds.do(printProducts())
         consumer = KafkaConsumer(topicName, group_id = 'group1',bootstrap_servers = bootstrap_servers,
                           auto_offset_reset = 'earliest')
         for message in consumer:
-            message = message.value
+            evento = bytes.decode(message.value)
+            query =("INSERT INTO Eventos (Productos) VALUES (%s)")
+            cursor.execute(query,(evento,))
+            mariadb_connection.commit()
+            print(evento)
             print(message)
 
 if __name__== "__main__":
-    main()
+
+    mariadb_connection,cursor = connectDatabase()
+
+    main(mariadb_connection,cursor)
